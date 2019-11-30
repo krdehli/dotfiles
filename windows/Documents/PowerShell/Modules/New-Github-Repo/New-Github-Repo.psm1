fac3a148942b221ce43aa694dc6bd21b0d00e634
Kim Roar Stenvard Dehli <krsdehli@gmail.com>
2019-11-30 01:42:46 +0100

    Updated Authentication method in Github-Repo creaton

--- windows/Documents/PowerShell/Modules/New-Github-Repo/New-Github-Repo.psm1
+++ windows/Documents/PowerShell/Modules/New-Github-Repo/New-Github-Repo.psm1
@@ -1,13 +1,18 @@
 function New-Github-Repo(
     [Parameter(Mandatory=$true)] [String] $Name,
-    [Parameter(Mandatory=$true)] [String] $Username,
-    [Parameter(Mandatory=$true)] [String] $Token,
+	[String] $Token,
     [String] $Private = $true
 ) {
+	if ((!$Token) -and ($Env:GITHUB_TOKEN)) {
+		$Token = $Env:GITHUB_TOKEN
+	} else {
+		throw "Parameter `"Token`" was missing and the GITHUB_TOKEN environment variable was not set"
+	}
     $data = @{name=$Name; private=$Private}
     $data | ConvertTo-Json -Compress | curl `
+		-H 'Accept: application/vnd.github.v3+json' `
         -H 'Content-Type: application/json' `
-        -u "${Username}:${Token}" `
+        -H "Authorization: token ${Token}" `
         https://api.github.com/user/repos `
         -d '@-' 
 }
