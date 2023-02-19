4d214c884210893985115d298f70e74e10c54f72
Kim Roar Stenvard Dehli <krsdehli@gmail.com>
2023-02-19 11:46:20 +0100

    Removed broken CMake integration of New-CppSourfile

--- windows/Documents/PowerShell/Modules/New-CppSourcefile/New-CppSourcefile.psm1
+++ windows/Documents/PowerShell/Modules/New-CppSourcefile/New-CppSourcefile.psm1
@@ -1,28 +1,12 @@
-function ToRelativePath([String] $RootPath, [String] $Path) {
-    $Segments = @()
-
-    $RootPath = (Join-Path $RootPath '')
-    $Path = (Join-Path $Path '')
-
-    if (-not ($Path.StartsWith($RootPath))) {
-        throw "Internal Error"
-    }
-
-    while (-not ($Path -eq $RootPath)) {
-        $Segments += (Split-Path -Leaf -Path $Path)
-        $Path = (Join-Path (Split-Path -Parent -Path $Path) '')
-    }
-    [array]::Reverse($Segments)
-    return (Join-Path @Segments)
-}
-
-function IncludeGuard([String] $TargetName, [String] $Namespace, [String] $Name) {
-    $Guard = $TargetName.ToUpper();
+function IncludeGuard( [String] $Name, [String] $TargetName = $null, [String] $Namespace = $null) {
+    $Guard = ($Name.ToUpper() + '_HPP_'+ ((New-Guid).Guid.ToUpper() -replace '-','_'))
+    
     if ($Namespace -and (-not ($Namespace -eq ''))) {
-        $Guard += ('_' + ($Namespace.ToUpper() -replace '::','_'))
+        $Guard = (($Namespace.ToUpper() -replace '::','_') + $Guard)
     }
-    $Guard += ('_' + $Name.ToUpper() + '_HPP')
-    $Guard += ('_' + ((New-Guid).Guid.ToUpper() -replace '-','_'))
+    if($TargetName -and (-not ($TargetName -eq ''))) {
+        $Guard = ($TargetName.ToUpper() + '_' + $Guard);
+    }
     return $Guard
 }
 
@@ -32,11 +16,8 @@ function New-CppSourcefile {
         [Parameter(Mandatory=$True, Position=0)]
         [ValidatePattern('^[a-zA-Z0-9_]+$')]
         [String] $Name,
-
-        [ValidateScript({ Test-Path -Path $_ -PathType Container })]
-        [Alias('Root','R')]
-        [String] $RootDirectory,
         
+        [Parameter(Mandatory=$True)]
         [ValidateScript({ Test-Path -Path $_ -PathType Container })]
         [Alias('Source','Src','S')]
         [String] $SourceDirectory,
@@ -47,39 +28,14 @@ function New-CppSourcefile {
 
         [Switch] $HeaderOnly,
 
-        [ValidateScript({ Test-Path -Path $_ -PathType Leaf })]
-        [String] $CMakeLists,
-
         [ValidatePattern('^(?:[a-zA-Z][a-zA-Z0-9_]*)(?:\:\:[a-zA-Z][a-zA-Z0-9_]*)*$')]
         [Alias('N')]
-        [String] $Namespace
+        [String] $Namespace,
+
+        [ValidatePattern('^[a-zA-Z0-9_]+$')]
+        [Alias('T')]
+        [String] $TargetName
     )
-
-    if (-not $RootDirectory) {
-        if (-not $CMakeLists) {
-            $RootDirectory = (Convert-Path '.')
-        }
-        else {
-            $RootDirectory = (Convert-Path (Split-Path -Path $CMakeLists -Parent))
-        }
-    }
-    else {
-        $RootDirectory = (Convert-Path $RootDirectory)
-    }
-
-    if (-not $CMakeLists) {
-        $CMakeLists = (Join-Path -Path $RootDirectory -ChildPath 'CMakeLists.txt')
-    }
-    else {
-        $CMakeLists = (Convert-Path $CMakeLists)
-    }
-
-    if (-not $SourceDirectory) {
-        $SourceDirectory = (Convert-Path (Join-Path -Path $RootDirectory -ChildPath 'src'))
-    }
-    else {
-        $SourceDirectory = (Convert-Path $SourceDirectory)
-    }
 
     if (-not $HeaderDirectory) {
         $HeaderDirectory = $SourceDirectory
@@ -88,12 +44,6 @@ function New-CppSourcefile {
         $HeaderDirectory = (Convert-Path $HeaderDirectory)
     }
 
-    if (-not (Test-Path -Path $RootDirectory -PathType Container)) {
-        throw "Could not find directory $RootDirectory"
-    }
-    if (-not (Test-Path -Path $CMakeLists -PathType Leaf)) {
-        throw "Could not find $CMakeLists"
-    }
     if (-not (Test-Path -Path $SourceDirectory -PathType Container)) {
         throw "Could not find directory $SourceDirectory"
     }
@@ -105,11 +55,8 @@ function New-CppSourcefile {
     $HeaderFileName = "${Name}.hpp"
     $SourceFilePath = (Join-Path -Path $SourceDirectory -ChildPath $SourceFileName)
     $HeaderFilePath = (Join-Path -Path $HeaderDirectory -ChildPath $HeaderFileName)
-    
-    $RelativeSourceFilePath = (ToRelativePath (Split-Path $CMakeLists -Parent) $SourceFilePath)
-    $RelativeHeaderFilePath = (ToRelativePath (Split-Path $CMakeLists -Parent) $HeaderFilePath)
 
-    $IncludeGuard = IncludeGuard -TargetName $TargetName -Namespace $Namespace -Name $Name
+    $IncludeGuard = IncludeGuard -Name $Name -TargetName $TargetName -Namespace $Namespace
 
     if (Test-Path $HeaderFilePath) {
         throw "Header file already exists"
@@ -144,27 +91,4 @@ function New-CppSourcefile {
             )
         }
     }
-
-    $NewCMakeContent = @()
-    $InsideAddTarget = $False
-    Get-Content -Path $CMakeLists | ForEach-Object {
-        $Line = $_
-        if ($Line -match 'add_(?:(?:executable)|(?:library))') {
-            $InsideAddTarget = $True
-        }
-        if ($InsideAddTarget -and ($Line -match 'INTERFACE')) {
-            $InsideAddTarget = $False
-        }
-        if ($InsideAddTarget -and ($Line -match '\)')) {
-            $Line = ($Line -replace '\)', '')
-            $NewCMakeContent += ("`t" + ($RelativeHeaderFilePath -replace '\\','/'))
-            if (-not $HeaderOnly) {
-                $NewCMakeContent += ("`t" + ($RelativeSourceFilePath -replace '\\','/'))
-            }
-            $NewCMakeContent += ')'
-            $InsideAddTarget = $False
-        }
-        $NewCMakeContent += $Line
-    }
-    Set-Content -Path $CMakeLists -Value $NewCMakeContent
 }
